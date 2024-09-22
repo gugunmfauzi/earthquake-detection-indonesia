e647626839e1023de55b80ffad433767539afca0
Acer <gugunmfauzi16@gmail.com>
2024-09-23 05:30:06 +0700

    release to pypi4

--- build-upload.bat
+++ build-upload.bat
@@ -1,3 +1,15 @@
 @echo off
-py -m build
-py -m twine upload --repository pypi dist/*
+
+rem Hapus file di folder dist/ jika ada
+echo Menghapus file lama di folder dist/...
+rmdir /s /q dist
+
+rem Build package baru
+echo Membuat distribusi package...
+python setup.py sdist bdist_wheel
+
+rem Upload package ke PyPI
+echo Mengunggah package ke PyPI...
+twine upload dist/*
+
+echo Proses selesai.
